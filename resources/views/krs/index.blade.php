233e3c05c8eff86e2a8c72ab125728ea2307425f
kellyzhang1808 <kellyzhang1808@gmail.com>
2022-11-12 11:55:56 +0700

    upload

---
+++ resources/views/krs/index.blade.php
@@ -0,0 +1,16 @@
+<?php
+    use App\Models\krs;
+    use App\Models\vmhskrs;
+?>
+@extends('layouts.app')
+
+@section('title')
+
+@section('content')
+    <h1>KRS Mahasiswa</h1>
+    <style>
+        table tr{
+            padding: 10px;
+        }
+    </style>
+@endsection
